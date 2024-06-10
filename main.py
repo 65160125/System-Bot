5eaf5a8d4c105608a393825e9e5ab3aa9a634a29
sath.s_ <156170104+65160125@users.noreply.github.com>
2024-06-10 14:27:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,32 @@
 import os
 import discord
 from discord.ext import commands
-from discord import app_commands
 import asyncio
 
 from myserver import server_on
 
-# Target user ID (replace with the actual user ID)
-TARGET_CHANNEL_ID = 1249285760713232424
-
-# Notification channel ID (replace with the actual channel ID for notifications)
+# รหัสช่องแจ้งเตือน (แทนที่ด้วยรหัสช่องจริง)
 NOTIFICATION_CHANNEL_ID = 1159149849539788932
 
 intents = discord.Intents.default()
 intents.members = True
 intents.voice_states = True
-intents.message_content = True  # Enable message content intent
+intents.message_content = True  # เปิดการใช้งาน intent การเนื้อหาข้อความ
 
 bot = commands.Bot(command_prefix='/', intents=intents)
-
-@bot.tree.command(name='hellobot', description='ไว้ให้บอททักทาย')
-async def hellocommand(interaction: discord.Interaction):
-    await interaction.response.send_message("Hello กูเป็นบอทของเซิฟซิสเต้ม")
-
-@bot.tree.command(name='name', description='ไว้ให้บอททักทายแต่มีชื่อด้วย')
-@app_commands.describe(name="คุณชื่ออะไร ?")
-async def namecommand(interaction: discord.Interaction, name: str):
-    await interaction.response.send_message(f"หวัดดีไอ{name}")
 
 @bot.event
 async def on_voice_state_update(member, before, after):
     # ตรวจสอบว่ามีการย้ายห้องเสียง
     if before.channel != after.channel:
         if before.channel is not None and after.channel is not None:
-            # Get the member's previous and current channels
+            # รับช่องเสียงก่อนหน้าและช่องเสียงปัจจุบันของสมาชิก
             before_channel = before.channel
             after_channel = after.channel
             
-            # Check for recent audit logs to find out who moved the member
-            async for entry in member.guild.audit_logs(limit=1, action=discord.AuditLogAction.member_move):
-                if entry.target and isinstance(entry.target, discord.Member) and entry.target.id == member.id:
+            # ตรวจสอบบันทึกการตรวจสอบล่าสุดเพื่อค้นหาว่าใครย้ายสมาชิก
+            async for entry in member.guild.audit_logs(limit=10, action=discord.AuditLogAction.member_move):
+                if entry.target.id == member.id:
                     mover = entry.user
                     notification_channel = bot.get_channel(NOTIFICATION_CHANNEL_ID)
                     if notification_channel:
