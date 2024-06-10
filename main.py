93345e8fad78ca32b203a96e9dde631200c785d0
sath.s_ <156170104+65160125@users.noreply.github.com>
2024-06-10 18:35:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,9 +48,14 @@ async def on_voice_state_update(member, before, after):
         if before.channel is not None and after.channel is not None:
             notification_channel = bot.get_channel(NOTIFICATION_CHANNEL_ID)
             if notification_channel:
+                # ดึงข้อมูลของผู้ที่ทำการย้าย
+                member_data = await member.guild.fetch_member(member.id)
+                # ดึงข้อมูลของผู้ที่ดำเนินการย้าย
+                mover_data = await member.guild.fetch_member(after.user.id)
                 await notification_channel.send(
-                    f'{member.name} ถูกย้ายจากห้อง {before.channel.name} ไปยังห้อง {after.channel.name} โดย {member.display_name}'
+                    f'{member_data.display_name} ถูกย้ายจากห้อง {before.channel.name} ไปยังห้อง {after.channel.name} โดย {mover_data.display_name}'
                 )
+
 
 server_on()
 
