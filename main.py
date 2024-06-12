44737830aca6bf0cb034113b7226fe9c4437bbe7
sath.s_ <156170104+65160125@users.noreply.github.com>
2024-06-12 23:58:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,28 @@ async def on_ready():
     await bot.tree.sync()
     print("Commands synced")
 
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.id == TARGET_USER_ID and after.channel:
+        wait_time = random.randint(1, 5)  # Wait for a random number of seconds between 100 and 120
+        await asyncio.sleep(wait_time)
+        if member.voice and member.voice.channel:
+            original_channel = after.channel  # Get the voice channel object
+            await member.edit(mute=True)  # Mute the user
+            # Create an embed message
+            embed = discord.Embed(
+                title="ภารกิจปิดเสียง",
+                description=f"ไอ <@{member.id}> ถูกปิดเสียง 😈.",
+                color=discord.Color.red()
+            )
+            embed.add_field(name="โดนปิดเสียงในห้อง", value=f"<#{original_channel.id}>", inline=True)
+            embed.add_field(name="เวลาที่เชื่อมต่อ", value=f"{wait_time} วินาที", inline=True)
+            embed.set_footer(text="ปิดเสียงคนปากหมาจำกัด")
+
+            # Send the embed message to the specified channel
+            notification_channel = bot.get_channel(NOTIFICATION_CHANNEL_ID)
+            if notification_channel:
+                await notification_channel.send(embed=embed)
 
 @bot.tree.command(name='hellobot', description='ไว้ให้บอททักทาย')
 async def hellocommand(interaction: discord.Interaction):
@@ -49,7 +71,7 @@ async def on_message(message):
         if mes == 'สวัสดี':
             await message.channel.send("สวัสดีจ้า " + str(message.author.name))
         else:
-            await message.channel.send("เราเพิ่งถูกพึ่งสร้างไม่กี่วัน อย่าคาดหวังให้มันพิมพ์อะไรเยอะสิ อีกอย่าง เราไม่ใช่ Ai ด้วย !!!")
+            await message.channel.send("เราเพิ่งถูกพึ่งสร้างวันนี้ อย่าคาดหวังให้มันพิมพ์อะไรเยอะสิ อีกอย่าง เราไม่ใช่ Ai ด้วย !!!")
 
 server_on()
 
